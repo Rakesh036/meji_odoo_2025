26ad2d1b873b29e9a15becf426696bec674b2215
milanpal969 <palmilan568@gmail.com>
2025-07-12 09:26:16 +0530

    apis setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return {
+    plugins: [react(),
+      tailwindcss(),
+    ],
+    server: {
+      proxy: {
+        "/api": {
+          target: process.env.VITE_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
+});
